f4eb8c56a91cd560f77135f1d2d2d58f0ccd3c63
Andres Portillo <andres.portillo@gmail.com>
2021-02-19 18:07:08 -0800

    Modify regex, support _ for pid field

--- lib/fluent/plugin/parser_logplex.rb
+++ lib/fluent/plugin/parser_logplex.rb
@@ -4,7 +4,7 @@ module Fluent
       # Parses syslog-formatted messages[1], framed using syslog TCP protocol octet counting framing method[2]
       # [1] https://tools.ietf.org/html/rfc5424#section-6
       # [2] https://tools.ietf.org/html/rfc6587#section-3.4.1
-      HTTPS_REGEXP = /^([0-9]+)\s+\<(?<pri>[0-9]+)\>[0-9]* (?<time>[^ ]*) (?<drain_id>[^ ]*) (?<ident>[a-zA-Z0-9_\/\.\-]*) (?<pid>[a-zA-Z0-9\.]+)? *- *(?<message>.*)$/
+      HTTPS_REGEXP = /^([0-9]+)\s+\<(?<pri>[0-9]+)\>[0-9]* (?<time>[^ ]*) (?<drain_id>[^ ]*) (?<ident>[a-zA-Z0-9_\/\.\-]*) (?<pid>[a-zA-Z0-9_\.]+)? *- *(?<message>.*)$/
 
       FACILITY_MAP = Fluent::Plugin::SyslogInput::FACILITY_MAP
       # Constant was renamed in 1.7.3.
