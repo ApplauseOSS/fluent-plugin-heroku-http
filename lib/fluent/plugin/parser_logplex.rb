b2b1ba2150558eeff6cd50b755047b3fa6d451b5
David Duthie <dave.a.duthie@gmail.com>
2022-01-26 12:35:14 +0200

    feat: handle dynatrace metadata

--- lib/fluent/plugin/parser_logplex.rb
+++ lib/fluent/plugin/parser_logplex.rb
@@ -4,7 +4,10 @@ module Fluent
       # Parses syslog-formatted messages[1], framed using syslog TCP protocol octet counting framing method[2]
       # [1] https://tools.ietf.org/html/rfc5424#section-6
       # [2] https://tools.ietf.org/html/rfc6587#section-3.4.1
-      HTTPS_REGEXP = /^([0-9]+)\s+\<(?<pri>[0-9]+)\>[0-9]* (?<time>[^ ]*) (?<drain_id>[^ ]*) (?<ident>[a-zA-Z0-9_\/\.\-]*) (?<pid>[a-zA-Z0-9\.]+)? *- *(?<message>.*)$/
+      HTTPS_REGEXP = %r{^(?<ignore>[0-9]+)\s+<(?<pri>[0-9]+)>[0-9]* (?<time>[^ ]*) (?<drain_id>[^ ]*) (?<ident>[a-zA-Z0-9_/.\-]*) (?<pid>[a-zA-Z0-9.]+)? *- *(?<message>.*)$}.freeze
+      DYNATRACE_FIELDS_REGEXP = /.*dt\.entity.host: (?<dt.entity.host>[^ ]+), dt\.entity.process_group_instance: (?<dt.entity.process_group_instance>[^ ]+), dt\.trace_id: (?<trace_id>[^ ]+), dt\.span_id: (?<span_id>[^ ]+)/.freeze
+      DYNATRACE_META_SECTION = /dt\.entity.host: ([^ ]+)?, dt\.entity.process_group_instance: ([^ ]+)?, dt\.trace_id: ([^ ]+)?, dt\.span_id: ([^ ]+)?/.freeze
+      LOGLEVEL_REGEXP = /(?<level>INFO|WARN|ERROR|FATAL)/.freeze
 
       FACILITY_MAP = Fluent::Plugin::SyslogInput::FACILITY_MAP
       # Constant was renamed in 1.7.3.
@@ -28,27 +31,57 @@ module Fluent
 
       config_param :with_priority, :bool, default: true
 
+      def parse_syslog(line)
+        m = line.match(HTTPS_REGEXP)
+
+        record = m.names.each_with_object({}) do |name, rec|
+          rec[name] = m[name]
+
+          # Process 'pri' field
+          next unless name == 'pri'
+
+          pri = m[name].to_i
+          # Split PRIVAL into Facility and Severity
+          rec['facility'] = FACILITY_MAP[pri >> FACILITY_SHIFT]
+          rec['loglevel'] = PRIORITY_MAP[pri & PRIORITY_MASK]
+        end
+        record.delete('pri')
+        record
+      end
+
+      def extract_dt_meta(record)
+        dt_match = record['message'].match(DYNATRACE_FIELDS_REGEXP)
+        dt_match&.named_captures&.each do |name, val|
+          record[name] = val
+        end
+        record
+      end
+
+      def override_loglevel(record)
+        loglevel_match = record['message'].match(LOGLEVEL_REGEXP)
+        record['loglevel'] = loglevel_match['level'] unless loglevel_match.nil?
+        record
+      end
+
+      def enforce_utf8(record)
+        record['message'] = record['message'].force_encoding('UTF-8')
+        record
+      end
+
+      def strip_dt_meta(record)
+        record['message'] = record['message'].gsub(DYNATRACE_META_SECTION, '')
+        record
+      end
+
       def parse(text)
-        expression = HTTPS_REGEXP
-
-        records =
-          text.split("\n").map do |line|
-            m = line.match(expression)
-
-            m.names.each_with_object({}) do |name, record|
-              record[name] = m[name]
-
-              # Process 'pri' field
-              next unless name == 'pri'
-              pri = m[name].to_i
-              record['pri'] = pri
-              # Split PRIVAL into Facility and Severity
-              record['facility'] = FACILITY_MAP[pri >> FACILITY_SHIFT]
-              record['priority'] = PRIORITY_MAP[pri & PRIORITY_MASK]
-            end
-          end
-
-        records.each { |record| record.delete('pri') }
+        records = text.split("\n").map do |line|
+          rec = parse_syslog(line)
+          rec = override_loglevel(rec)
+          rec = enforce_utf8(rec)
+          rec = extract_dt_meta(rec)
+          rec = strip_dt_meta(rec)
+          rec
+        end
         yield nil, records
       end
     end
