446ff106b0033938f3e05aaa10e6b5bac398012d
David Duthie <dave.a.duthie@gmail.com>
2022-03-18 15:21:38 +0200

    fix(parser): tweak dynatrace regexes

--- test/plugin/test_parser_logplex.rb
+++ test/plugin/test_parser_logplex.rb
@@ -36,14 +36,35 @@ class ParserLogplexTest < Test::Unit::TestCase
     end
   end
 
+  keys_to_extract = ['dt.entity.host', 'dt.entity.process_group_instance', 'trace_id', 'span_id']
+
   sub_test_case 'plugin will parse dynatrace metadata' do
-    keys_to_extract = ['dt.entity.host', 'dt.entity.process_group_instance', 'trace_id', 'span_id']
     test 'record has dynatrace metadata' do
       d = create_driver(CONFIG)
       text = '59 <13>1 2022-01-26T06:25:52.589365+00:00 host app web.1 - dt.entity.host: 12345, dt.entity.process_group_instance: 4321, dt.trace_id: abcd, dt.span_id: dcba - more stuff here'
       d.instance.parse(text) do |_time, records|
         assert_equal(
-          [{ 'message' => '- more stuff here' }],
+          [{ 'message' => 'more stuff here' }],
+          records.map { |record| record.select { |key, _| ['message'].include? key } }
+        )
+        assert_equal(
+          [{ 'dt.entity.host' => '12345',
+             'dt.entity.process_group_instance' => '4321',
+             'trace_id' => 'abcd',
+             'span_id' => 'dcba' }],
+          records.map { |record| record.select { |key, _| keys_to_extract.include? key } }
+        )
+      end
+    end
+  end
+
+  sub_test_case 'plugin will parse dynatrace metadata in another order, and with unexpected key value pairs present' do
+    test '' do
+      d = create_driver(CONFIG)
+      text = '59 <13>1 2022-01-26T06:25:52.589365+00:00 host app web.1 - dt.entity.process_group_instance: 4321, dt.trace_sampled: true, dt.trace_id: abcd, dt.span_id: dcba, dt.entity.host: 12345 - more stuff here'
+      d.instance.parse(text) do |_time, records|
+        assert_equal(
+          [{ 'message' => 'more stuff here' }],
           records.map { |record| record.select { |key, _| ['message'].include? key } }
         )
         assert_equal(
