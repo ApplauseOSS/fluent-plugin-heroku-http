0cbb63792ae186dc4997d39af1a297a0cae18cea
David Duthie <dave.a.duthie@gmail.com>
2022-04-28 10:08:03 +0200

    experiment: try move parsers into parse section

--- lib/fluent/plugin/in_heroku_http.rb
+++ lib/fluent/plugin/in_heroku_http.rb
@@ -7,11 +7,6 @@ module Fluent
     # FIXME: what to do with this?
     class ContentTypeParser < Parser
       Fluent::Plugin.register_parser('content_type_parser', self)
-    end
-
-    # FIXME: rename
-    class HerokuHttpInput < HttpInput
-      Fluent::Plugin.register_input('heroku_http', self)
 
       config_section :parser, multi: true, required: true do
         config_param :content_type, :string
@@ -31,15 +26,33 @@ module Fluent
           parser.configure(parser_config.corresponding_config_element)
           @content_type_parsers[parser_config.content_type] = parser
         end
+
+        puts 'Content type parsers:'
+        puts @content_type_parsers.to_s
+      end
+
+      def parse(content_type, text)
+        parser_for_type = @content_type_parsers[content_type]
+        time, records = parser_for_type.parse(text) { |time, records| return time, records }
+        yield time, records
+      end
+    end
+
+    # FIXME: rename
+    class HerokuHttpInput < HttpInput
+      Fluent::Plugin.register_input('heroku_http', self)
+
+      helpers :parser
+
+      def configure(conf)
+        super
+        @parser = parser_create
       end
 
       def parse_params_with_parser(params)
         content_type = params['HTTP_CONTENT_TYPE']
-        parser = @content_type_parsers[content_type]
-        tyme, records = parser.parse(params['_event_record']) do |time, records|
-          return time, records
-        end
-
+        payload = params['_event_record']
+        tyme, records = @parser.parse(content_type, payload) { |time, records| return time, records }
         [tyme, records]
       end
     end
